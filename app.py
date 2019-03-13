bc2da25870bdb9ce1d0ca1923fcc5669628e5eae
Alisa Qian <alisa@Alisas-MacBook.wifi-campus.warwick.ac.uk>
2019-03-13 19:17:01 +0000

    Added new files to help with deployment

---
+++ app.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, render_template, request
+import requests
+
+def send_simple_message(email,name):
+	return requests.post(
+		"https://api.mailgun.net/v3/sandbox89f291fedc7243d38414d6871efcab53.mailgun.org/messages",
+		auth=("api", "675d7587416512d36fb5bc88ac45cb27-acb0b40c-81fd1365"),
+		data={"from": "Mailgun Sandbox <postmaster@sandbox89f291fedc7243d38414d6871efcab53.mailgun.org>",
+			"to": email,
+			"subject": name,
+			"text": "Congratulations, you just sent an email with Mailgun! You are truly awesome!"})
+
+
+app = Flask("cosmicbridge")
+
+@app.route("/")
+def say_hello():
+    return render_template("index.html")
+
+@app.route("/models")
+def model_page():
+	return render_template("models.html")
+
+@app.route("/about")
+def about_page():
+	return render_template("about.html")
+
+@app.route("/photographers")
+def photographers_page():
+	return render_template("photographers.html")
+
+@app.route("/contact")
+def contact_page():
+    return render_template("contact.html")
+
+@app.route("/confirmation", methods=["POST"])
+def send_email():
+    data = request.values
+    send_simple_message(data['email'],data['name'])
+    return render_template("confirmation.html",form_data=data)
+
+if __name__ == '__main__':
+	app.run(debug=True)
